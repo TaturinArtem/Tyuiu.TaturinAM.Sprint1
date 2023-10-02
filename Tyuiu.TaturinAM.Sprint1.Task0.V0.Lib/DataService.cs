a54548806ad94d3c1f60d6780f452b14983fa03a
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-10-02 16:28:18 +0500

    Task0
    Task0

---
+++ Tyuiu.TaturinAM.Sprint1.Task0.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TaturinAM.Sprint1.Task0.V0.Lib
+{
+    public class DataService : ISprint1Task0V15
+    {
+        public double Calculate()
+        {
+            return 20 - (2 * 2 - 8);
+        }
+    }
+}
---
+++ Tyuiu.TaturinAM.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TaturinAM.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.TaturinAM.Sprint1.Task0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Татурин А. М. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                              *");
+            Console.WriteLine("* Татурин Артем Максимович | СМАРТб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значения выражения                *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 20 - (2 * 2 - 8)                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
