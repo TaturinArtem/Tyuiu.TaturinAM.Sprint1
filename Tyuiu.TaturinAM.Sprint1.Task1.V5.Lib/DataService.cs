9dcb1ca0f709264de2eb09ec0b1f649fcb630a96
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-10-03 19:05:24 +0500

    Task1
    Task1

---
+++ Tyuiu.TaturinAM.Sprint1.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TaturinAM.Sprint1.Task1.V5.Lib
+{
+    public class DataService : ISprint1Task1V5
+    {
+        public double Calculate(double x, double y)
+        {
+            return (5 + (x * y / 2));
+        }
+    }
+}
---
+++ Tyuiu.TaturinAM.Sprint1.Task1.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TaturinAM.Sprint1.Task1.V5.Lib;
+namespace Tyuiu.TaturinAM.Sprint1.Task1.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Татурин А. М. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода-вывода в консльных приложениях                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Татурин Артем Максимович | СМАРТб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (5+(x*y/2)) и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (5+(x*y/2))                                                             *");
+            Console.WriteLine("***************************************************************************");
+          
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
